beda7e27e5654747d4a07e8b35d756eaadb6735c
Richard Moseley <richard.moseley4@gmail.com>
2024-08-21 18:39:09 +0100

    Update test_11 for read changes
    Modify the test_11 to cater for the change in the calling sequence of the read()
    method, otherwise it triggers an error about trying to reposition using a
    directional mode (ISNEXT, ISPREV, or ISCURR)

--- tests/test_11.py
+++ tests/test_11.py
@@ -7,24 +7,25 @@ from pyisam.table import ISAMtable
 from pyisam.tabdefns.stxtables import DEFILEdefn
 
 class ISAMtableIter:
-  def __init__(self, tabobj, index=None, mode=None, *args, **kwds):
+  def __init__(self, tabobj, *args, **kwds):
     self._tabobj = tabobj
-    self._index = index
-    self._mode = ReadMode(mode)
     self._args = args
     self._kwds = kwds
 
   def __iter__(self):
-    self._tabobj.open()
+    self._first = True
     return self
 
   def __next__(self):
     try:
-      return self._tabobj.read(index=self._index, mode=self._mode, *self._args, **self._kwds)
+      if self._first:
+        self._first = False
+        return self._tabobj.read(*self._args, **self._kwds)
+      return self._tabobj.read()
     except IsamEndFile:
       raise StopIteration
 
 def test(opts):
   DEFILE = ISAMtable(DEFILEdefn, tabpath=opts.tstdata, mode=OpenMode.ISINPUT)
-  for row in ISAMtableIter(DEFILE, mode=ReadMode.ISPREV, filename='defile'):
+  for row in ISAMtableIter(DEFILE, ReadMode.ISGREAT, filename='defile'):
     print(row)
